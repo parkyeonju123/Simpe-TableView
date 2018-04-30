f8d01bb23cad178790d75817b09640e2845b2185
Jonghyun Kim <jhkim@dit.ac.kr>
2018-04-30 11:50:03 +0900

    UITableView를 사용하여 simple TableView 만들기

--- Simple TableView/ViewController.swift
+++ Simple TableView/ViewController.swift
@@ -8,18 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var myTableView: UITableView!
+    var animals = ["Cow", "Pig", "Dog", "Rabbit", "Bird"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // UIViewController와 Delegate 연결
+        myTableView.delegate = self
+        myTableView.dataSource = self
+    
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // UITableViewDataSoure
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return animals.count
     }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let identifier = "Cell"
+        let cell = myTableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath)
 
+        cell.textLabel?.text = animals[indexPath.row]
+        return cell
+    }
 
 }
 
